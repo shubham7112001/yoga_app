0ed5de06a4ef0aaba1f172735a72291f7e3f3e8c
Shubham Tiwari <st7112001@gmail.com>
2023-03-27 23:57:59 +0530

    Are You Ready Page UI

--- lib/Screens/are_you_ready.dart
+++ lib/Screens/are_you_ready.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../Utils/app_colors.dart';
+
 class AreYouReady extends StatelessWidget {
   const AreYouReady({Key? key}) : super(key: key);
 
@@ -11,14 +13,46 @@ class AreYouReady extends StatelessWidget {
     return ChangeNotifierProvider<TimerModel>(
         create: (context)=>TimerModel(),
       child: Scaffold(
-        body: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text("ARE YOU READY",style: TextStyle(fontSize: 60),),
-              Consumer<TimerModel>(builder: (context,myModel,child)=>Text("${myModel.countdown}"))
-            ],
+        body: Center(
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: MediaQuery.of(context).size.height/2 - 100,),
+                Text("ARE YOU\n READY",
+                textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 55,
+                        fontFamily: 'sanFrancis',
+
+                        fontWeight: FontWeight.w900),),
+                SizedBox(height: 25,),
+                Consumer<TimerModel>(
+                    builder: (context,myModel,child)=>Text(
+                        "0${myModel.countdown}",
+                        style: TextStyle(
+                            fontSize: 66,
+                            fontFamily: 'SanFrancisco',
+                            fontWeight: FontWeight.w900,
+                            color: AppColors.appBarColor))),
+                Spacer(),
+                Divider(thickness: 2,),
+                Align(
+                  widthFactor: 4,
+                  alignment: Alignment.bottomLeft,
+                  child: Container(
+                    padding: EdgeInsets.only(left: 10,bottom: 15),
+                    child: Text("Next : Anulom Vilom",
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontFamily: 'SanFrancisco',
+                        fontWeight: FontWeight.w200),),
+                  ),
+                )
+
+              ],
+            ),
           ),
         ),
       ),
