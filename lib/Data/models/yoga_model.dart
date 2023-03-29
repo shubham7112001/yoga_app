2db47de590df3b7414c27735b7202d62b5797932
Shubham Tiwari <st7112001@gmail.com>
2023-03-30 01:06:24 +0530

    Yoga model Created

--- lib/Data/models/yoga_model.dart
+++ lib/Data/models/yoga_model.dart
@@ -5,3 +5,48 @@ class YogaModel{
   static String ImageName = "ImageName";
 
 }
+
+class Yoga{
+  final int? id;
+  final bool Seconds;
+  final String YogaTitle;
+  final String YogaImgUrl;
+
+  const Yoga({
+    this.id,
+    required this.Seconds,
+    required this.YogaTitle,
+    required this.YogaImgUrl
+});
+
+  Yoga copy({
+    int? id,
+    bool? Seconds,
+    String? YogaTitle,
+    String? YogaImgUrl}){
+      return Yoga(
+        id: id ?? this.id,
+        Seconds: Seconds ?? this.Seconds,
+        YogaImgUrl: YogaImgUrl ?? this.YogaImgUrl,
+        YogaTitle: YogaTitle ?? this.YogaTitle
+      );
+    }
+
+    static Yoga fromJson(Map<String,Object?> json){
+    return Yoga(
+      id: json[YogaModel.IdName] as int?,
+      Seconds: json[YogaModel.SecondsOrNot] == 1,
+      YogaImgUrl: json[YogaModel.ImageName] as String,
+      YogaTitle: json[YogaModel.YogaName] as String
+      );
+    }
+
+    Map<String, Object?> toJson(){
+    return {
+      YogaModel.IdName : id,
+      YogaModel.SecondsOrNot : Seconds ? 1 : 0,
+      YogaModel.YogaName : YogaTitle,
+      YogaModel.ImageName : YogaImgUrl
+       };
+    }
+}
--- lib/services/yoga_db.dart
+++ lib/services/yoga_db.dart
@@ -1,5 +1,6 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:yoga_app/Data/models/yoga_model.dart';
 
 class YogaDatabase{
   static final YogaDatabase instance = YogaDatabase._init();
@@ -10,10 +11,16 @@ class YogaDatabase{
     final dbPath = await getDatabasesPath();
     final path  = join(dbPath,filePath);
 
-    return await openDatabase(path,version: 1);
+    return await openDatabase(path,version: 1,onCreate: _createDB);
   }
 
-  _createDB(Database db, int version){
+  Future<Database?> get database async{
+    if(_database != null) return _database;
+    _database = await _initializeDB('YogaStepsDB.db');
+    return _database;
+  }
+
+  Future _createDB(Database db, int version) async{
     final idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
     final textType = 'TEXT NOT NULL';
     final boolType = 'BOOLEAN NOT NULL';
@@ -21,10 +28,13 @@ class YogaDatabase{
 
     // id, bool(Seconds), text
 
-    db.execute('''
-    CREATE TABLE BeginnerYoga{
-    
-    }
+    await db.execute('''
+    CREATE TABLE BeginnerYoga(
+    ${YogaModel.IdName} $idType,
+    ${YogaModel.YogaName} $textType,
+    ${YogaModel.ImageName} $textType,
+    ${YogaModel.SecondsOrNot} $boolType
+    )
     ''');
 
   }
